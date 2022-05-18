a1b42f718d4f2d741a6d2a896bbffd4a0aeaf8a5
ValdirRoncato <104869163+ValdirRoncato@users.noreply.github.com>
2022-05-18 20:17:44 -0300

    20220518-2017-projeto financeiro

---
+++ src/main/java/com/vroncato/financeiro/app/FinanceiroAppApplication.java
@@ -0,0 +1,13 @@
+package com.vroncato.financeiro.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroAppApplication.class, args);
+	}
+
+}
